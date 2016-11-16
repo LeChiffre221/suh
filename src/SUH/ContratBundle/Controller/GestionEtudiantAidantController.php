9907538b8c6b5f7480a37e5a2ca195f4fb341a6b
Pierre Couy Perrais <picouyperr@dpt_info.iut.local>
2016-11-16 17:09:11 +0100

    userEtudiantAidant

--- src/SUH/ContratBundle/Controller/GestionEtudiantAidantController.php
+++ src/SUH/ContratBundle/Controller/GestionEtudiantAidantController.php
@@ -20,11 +20,43 @@ class GestionEtudiantAidantController extends Controller
         $etudiantassiste = array();
         $etudiantAidant = new EtudiantAidant();
 
+        /* formulaire */
+
         $form = $this->get('form.factory')->create(new EtudiantAidantType(), $etudiantAidant);
 
         if ($form->handleRequest($request)->isValid()) {
 
 	      $em = $this->getDoctrine()->getManager();
+
+        /*génération pass/user*/
+
+            //Récupération données formulaire POST
+            $nom = $request->request->get('nom');
+            $prenom = $request->request->get('prenom');
+            $username = $nom.charAt(0) + $prenom.charAt(0);
+            $pass = substr( md5(rand()), 0, 5);
+
+            //Si il n'y a pas déjà un utilisateur avec le même identifiant
+            $userRepository = $em->getRepository('SUHConnexionBundle:User');
+            $a = $userRepository->findByUsername($username);
+            if (empty($a)) {
+                //Création d'un nouvel utilisateur
+                $user = new \SUH\ConnexionBundle\Entity\User();
+
+                //Encodage du mot de passe (sha512, converti en base64, 5000itérations)
+                $factory = $this->get('security.encoder_factory');
+                $encoder = $factory->getEncoder($user);
+                $password = $encoder->encodePassword($pass, $user->getSalt());
+
+                $user->setUsername($username);
+                $user->setPassword($password);
+                $user->setSalt('');
+                $user->setRoles(array('ROLE_USER'));
+
+                $em->persist($user);
+            }
+
+
 	      $em->persist($etudiantAidant);
 	      $em->flush();
 	      $request->getSession()->getFlashBag()->add('notice', 'EtudiantAidant inscrit');
