8408002838c11d111464e641c062d0b7e2d757b1
Pierre Couy Perrais <picouyperr@dpt_info.iut.local>
2016-11-25 09:01:57 +0100

    hue hue

---
+++ src/SUH/ContratBundle/Controller/RepositoryFunctions.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace SUH\ContratBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
+
+class RepositoryFunctionsController extends Controller
+{
+
+
+
+}
