fca85b82db95999671d7c9148268e8705955a47b
mathieu gaume <lechi@DESKTOP-QRL5LSB.(none)>
2016-11-30 15:50:59 +0100

    ...

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit8315d1b9da489bda56549b06687fd230::getLoader();
+return ComposerAutoloaderInit05d46c9de5acaf8023e3bd2e5b789f73::getLoader();
