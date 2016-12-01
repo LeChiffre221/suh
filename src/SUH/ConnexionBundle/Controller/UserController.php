36d6f2b668674b38dffa55cc24186b56b9349af8
Pierre Couy Perrais <picouyperr@dpt_info.iut.local>
2016-12-01 16:40:51 +0100

    gestions users

--- src/SUH/ConnexionBundle/Controller/UserController.php
+++ src/SUH/ConnexionBundle/Controller/UserController.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: lechi
- * Date: 25/11/2016
- * Time: 14:15
- */
 
 namespace SUH\ConnexionBundle\Controller;
 
@@ -15,31 +9,65 @@ use SUH\ConnexionBundle\SUHConnexionBundle;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+
 class UserController extends Controller
 {
     public function afficherListeUtilisateurAction(Request $request){
+
         $em = $this->getDoctrine()->getManager();
         $listeUtilisateur = $em->getRepository('SUHConnexionBundle:User')->findByRole("ROLE_ADMIN");
 
-        $user = new User();
+        $user = new \SUH\ConnexionBundle\Entity\User();
+        
+
         $form = $this->get('form.factory')->create(new UtilisateurType, $user);
         $form->remove('connexion');
 
         if ($form->handleRequest($request)->isValid()) {
 
-            $user->setRoles(array("ROLE_ADMIN"));
-            $user->setSalt("");
+            
+            $pass = $user->getPassword();
+            $username = $user->getUsername();
 
+            $i= 0;
+            $usernameTmp = $username;
+
+            do{
+                if($i != 0){
+                    $username = $usernameTmp.$i;
+                }
+
+                $userRepository = $em->getRepository('SUHConnexionBundle:User');
+                $user = $userRepository->findByUsername($username);
+
+                $i++;
+
+            }while(!empty($user));
+
+
+            //Encodage du mot de passe (sha512, converti en base64, 5000itérations)
+            $user = new \SUH\ConnexionBundle\Entity\User();
+            $factory = $this->get('security.encoder_factory');
+            $encoder = $factory->getEncoder($user);
+            $password = $encoder->encodePassword($pass, '');
+
+            $user->setUsername($username);
+            $user->setPassword($password);
+            $user->setSalt('');
+            $user->setRoles(array("ROLE_ADMIN"));
             $em->persist($user);
             $em->flush();
 
-            $request->getSession()->getFlashBag()->add('notice', 'Administrateur ajouté !');
+            $request->getSession()->getFlashBag()->add('notice', $pass);
+
+            return $this->redirect($this->generateUrl('suh_user_homepage'));
 
         }
 
-        foreach ($user as $listeUtilisateur){
-            var_dump($user['roles']);
-        }
+        // foreach ($user as $listeUtilisateur){
+        //     var_dump($user['roles']);
+        // }
 
         return $this->render('SUHConnexionBundle:Connexion:gestionUser.html.twig', array(
             'form' => $form->createView(),
@@ -51,14 +79,13 @@ class UserController extends Controller
     /**
      * @Security("has_role('ROLE_SUPER_ADMIN')")
      */
-    public function deleteUser(Request $request, $idUser){
+    public function deleteUserAction(Request $request, $idUser){
         $em = $this->getDoctrine()->getManager();
 
         $user = $em->getRepository("SUHConnexionBundle:User")->find($idUser);
         $em->remove($user);
-        $em->persist($user);
         $em->flush();
-
+        $request->getSession()->getFlashBag()->add('notice', 'Administrateur supprimé !');
         return $this->redirectToRoute("suh_user_homepage");
 
     }
